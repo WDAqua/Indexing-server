0fc82cb88517509f598710da105de8816f53f742
Dennis Diefenbach <dennis.diefenbach@univ-st-etienne.fr>
2016-10-18 17:02:02 +0200

    Refactoring plus limitation to ingoing

--- src/main/java/eu/wdaqua/core0/server/Index.java
+++ src/main/java/eu/wdaqua/core0/server/Index.java
@@ -28,10 +28,12 @@ import org.apache.log4j.BasicConfigurator;
 import com.google.common.collect.Table;
 import com.google.common.collect.HashBasedTable;
 
+import static java.lang.Integer.min;
+
 public class Index {
 	
 	String[] dump = new String[1];
-	
+
 	private BiMap<String, Integer> map = HashBiMap.create();
 	private BiMap<String, Integer> mapRelation = HashBiMap.create();
 	private int rowI;
@@ -48,125 +50,119 @@ public class Index {
 		for (int l=0; l < uris.length; l++){
 			urisHash.put(uris[l],l);
 		}
-		
+
 		long startTime = System.currentTimeMillis();
 		Connection c = new Connection();
 		int i=0;
-        ArrayList<String> results=new ArrayList<String>();
-        HashSet<String> relations = new HashSet<String>();
-	for (int v = 0; v < uris.length; v++) {
-        	if (map.containsKey(uris[v])){
-		String s=uris[v].replace("http://dbpedia.org/ontology/","");
-                if (Character.isUpperCase(s.charAt(0))==false && uris[v].contains("http://wdaqua/")==false){ //Check that it is not a class
-	        	int n=map.get(uris[v]);
-	                for (int w=0; w < g.adj_out(n).size(); w++) {
-	                	int next = g.adj_out(n).get(w);
-				String edge1=mapRelation.inverse().get(g.edge_out(n).get(w));
-				boolean edge1_found=false;
-	                	relations.add(edge1);
-				String node1=map.inverse().get(g.adj_out(n).get(w));
-				boolean node1_found=false;
-	                	if (urisHash.containsKey(edge1)){
-	                		c.add(v, urisHash.get(edge1), 1);
-					edge1_found=true;
-	                	}
-	                	if (urisHash.containsKey(node1)){
-	                		c.add(v, urisHash.get(node1), 2);
-					node1_found=true;
-	                	}
-	                	for (int l=0; l<g.adj_out(next).size(); l++){
-	                    		String edge2=mapRelation.inverse().get(g.edge_out(next).get(l));
-	                    		String node2=map.inverse().get(g.adj_out(next).get(l));
-	                    		i++;
-	                    		if (urisHash.containsKey(edge2)){
+		ArrayList<String> results=new ArrayList<String>();
+		HashSet<String> relations = new HashSet<String>();
+		for (int v = 0; v < uris.length; v++) {
+			if (map.containsKey(uris[v])){
+				String s=uris[v].replace("http://dbpedia.org/ontology/","");
+				if (Character.isUpperCase(s.charAt(0))==false && uris[v].contains("http://wdaqua/")==false){ //Check that it is not a class
+				int n=map.get(uris[v]);
+					for (int w=0; w < g.adj_out(n).size(); w++) {
+						int next = g.adj_out(n).get(w);
+						String edge1=mapRelation.inverse().get(g.edge_out(n).get(w));
+						boolean edge1_found=false;
+								relations.add(edge1);
+						String node1=map.inverse().get(g.adj_out(n).get(w));
+						if (urisHash.containsKey(edge1)){
+							c.add(v, urisHash.get(edge1), 1);
+							edge1_found=true;
+						}
+						if (urisHash.containsKey(node1)){
+							c.add(v, urisHash.get(node1), 2);
+						}
+						for (int l=0; l<g.adj_out(next).size(); l++){
+								String edge2=mapRelation.inverse().get(g.edge_out(next).get(l));
+								String node2=map.inverse().get(g.adj_out(next).get(l));
+								i++;
+								if (urisHash.containsKey(edge2)){
 						c.add(v, urisHash.get(edge2), 3);
 						if (edge1_found==true){
 							c.add(urisHash.get(edge1), urisHash.get(edge2), 2);
 						}
-		                	}
-		                	if (urisHash.containsKey(node2)){
-						c.add(v, urisHash.get(node2), 4);
-						if (edge1_found==true){
-							c.add(urisHash.get(edge1), urisHash.get(node2), 3);
-                                                }
-		                	}
-	                    	}
-				/*
-				//Go back but not for classes
-				if (node1!=null){
-				s=node1.replace("http://dbpedia.org/ontology/","");
-                		if (Character.isUpperCase(s.charAt(0))==false){
-	                   		for (int l=0; l<g.adj_in(w).size(); l++){
-						String edge=mapRelation.inverse().get(g.edge_out(n).get(w));
-						String node=map.inverse().get(g.adj_out(n).get(w));
-						i++;
-	                    			if (urisHash.containsKey(edge)){
-		                			B.setEntry(v, urisHash.get(edge), -3.0);
-		                		}
-		                		if (urisHash.containsKey(node)){
-		                			B.setEntry(v, urisHash.get(node), -4.0);
-		                		}
-	                    		}
-				}
-				}*/
-	                }
-	                for (int w=0; w<g.adj_in(n).size(); w++){
-	                	int next = g.adj_in(n).get(w);
-				String edge1=mapRelation.inverse().get(g.edge_in(n).get(w));
-	                	boolean edge1_found=false;
-				relations.add(edge1);
-				String node1=map.inverse().get(g.adj_in(n).get(w));
-	                	if (urisHash.containsKey(edge1)){
-					c.add(v, urisHash.get(edge1), -1);
-					edge1_found=true;
-	                	}
-	                	if (urisHash.containsKey(node1)){
-	                		c.add(v, urisHash.get(node1), -2);
-	                	}
-	                	i++;
-                    		//results.add("<  "+edge1+" --- "+node1);
-                    		for (int l=0; l<g.adj_out(next).size(); l++){
-	                    		String edge2=mapRelation.inverse().get(g.edge_out(next).get(l));
-	                    		String node2=map.inverse().get(g.adj_out(next).get(l));
-                    			i++;
-	                    		if (urisHash.containsKey(edge2)){
-		                		c.add(v, urisHash.get(edge2), -3);
-						if (edge1_found==true){
-							c.add(urisHash.get(edge1), urisHash.get(edge2), -2);
+							}
+							if (urisHash.containsKey(node2)){
+								c.add(v, urisHash.get(node2), 4);
+								if (edge1_found==true){
+									c.add(urisHash.get(edge1), urisHash.get(node2), 3);
+								}
+							}
 						}
-		                	}
-	                    		if (urisHash.containsKey(node2)){
-						c.add(v, urisHash.get(node2), -4);
-						if (edge1_found==true){
-							c.add(urisHash.get(edge1), urisHash.get(node2), -3);
-                                                }
-		                	}
-	                    		//results.add("<<    "+edge2+" --- "+node2);
-	                	}
-                    	}
-	        	}
+						/*
+						//Go back but not for classes
+						if (node1!=null){
+						s=node1.replace("http://dbpedia.org/ontology/","");
+								if (Character.isUpperCase(s.charAt(0))==false){
+									for (int l=0; l<g.adj_in(w).size(); l++){
+								String edge=mapRelation.inverse().get(g.edge_out(n).get(w));
+								String node=map.inverse().get(g.adj_out(n).get(w));
+								i++;
+											if (urisHash.containsKey(edge)){
+											B.setEntry(v, urisHash.get(edge), -3.0);
+										}
+										if (urisHash.containsKey(node)){
+											B.setEntry(v, urisHash.get(node), -4.0);
+										}
+										}
+						}
+						}*/
+					}
+					//limits the search to 5000 ingoing edges, to huge for classes
+					for (int w=0; w<min(g.adj_in(n).size(),5000); w++){
+						int next = g.adj_in(n).get(w);
+						String edge1=mapRelation.inverse().get(g.edge_in(n).get(w));
+						boolean edge1_found=false;
+						relations.add(edge1);
+						String node1=map.inverse().get(g.adj_in(n).get(w));
+						if (urisHash.containsKey(edge1)){
+							c.add(v, urisHash.get(edge1), -1);
+							edge1_found=true;
+						}
+						if (urisHash.containsKey(node1)){
+							c.add(v, urisHash.get(node1), -2);
+						}
+						i++;
+						for (int l=0; l<g.adj_out(next).size(); l++){
+							String edge2=mapRelation.inverse().get(g.edge_out(next).get(l));
+							String node2=map.inverse().get(g.adj_out(next).get(l));
+							i++;
+							if (urisHash.containsKey(edge2)){
+								c.add(v, urisHash.get(edge2), -3);
+								if (edge1_found==true){
+									c.add(urisHash.get(edge1), urisHash.get(edge2), -2);
+								}
+							}
+							if (urisHash.containsKey(node2)){
+								c.add(v, urisHash.get(node2), -4);
+								if (edge1_found==true){
+									c.add(urisHash.get(edge1), urisHash.get(node2), -3);
+								}
+							}
+						}
+					}
+				}
 			}
-	        }
-	        long estimatedTime = System.currentTimeMillis() - startTime;
-	        for (String s: results){
-	        	System.out.println(s);
-	        }
-	        System.out.println("Time "+estimatedTime);
-	        System.out.println("Traversed edges "+i);
-	        System.out.println("Number relations "+relations.size());
-        return c;
+			}
+			long estimatedTime = System.currentTimeMillis() - startTime;
+			System.out.println("Time "+estimatedTime);
+			System.out.println("Traversed edges "+i);
+			System.out.println("Number relations "+relations.size());
+			return c;
 	}
-	
-	
+
+
 	public Integer get(String URI) throws IOException, ClassNotFoundException{
 		return (Integer) map.get(URI);
 	}
-	
-	
+
+
 	public String get(Integer i) throws IOException, ClassNotFoundException{
 		return (String)map.inverse().get(i);
 	}
-	
+
 	public void index() throws IOException, ClassNotFoundException{
 		//Add some uri for literals
 		Integer i=1;
@@ -214,8 +210,8 @@ public class Index {
 				}
 			}
 		}
-        System.out.println("Number resources: "+i);
-        System.out.println("Number relations: "+r);
+		System.out.println("Number resources: "+i);
+		System.out.println("Number relations: "+r);
 
 		g = new Digraph(i);
 		g.addEdge(map.get("http://wdaqua/dateLiteral"), mapRelation.get("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"), map.get("http://wdaqua/Date"));
@@ -263,7 +259,7 @@ public class Index {
 		}
 		rowI=i;
 		rowR=r;
- 		System.out.println("Number triples "+j);    
+		System.out.println("Number triples "+j);
 	}
 
 	public static PipedRDFIterator<Triple> parse(final String dump){
@@ -284,6 +280,5 @@ public class Index {
 		executor.submit(parser);
 		return iter;
 	}
-
 }
 
