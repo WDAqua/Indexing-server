bc97c8bbceacc422773bd5edd915f9bb3a2379da
dd77474h <dennis.diefenbach@univ-st-etienne.fr>
2016-10-21 13:53:00 +0200

    Script updated for dbpedia 2016

--- init.sh
+++ init.sh
@@ -1,12 +1,12 @@
-#Download part of DBpedia 2015
-wget http://downloads.dbpedia.org/2015-04/core-i18n/en/instance-types_en.nt.bz2
-wget http://downloads.dbpedia.org/2015-04/core-i18n/en/mappingbased-properties_en.nt.bz2
-wget http://downloads.dbpedia.org/2015-04/core-i18n/en/labels_en.nt.bz2
+#Download part of DBpedia 2016
+wget http://downloads.dbpedia.org/2015-10/core-i18n/en/instance_types_en.ttl.bz2
+wget http://downloads.dbpedia.org/2015-10/core-i18n/en/mappingbased_objects_en.ttl.bz2
+wget http://downloads.dbpedia.org/2015-10/core-i18n/en/infobox_properties_en.ttl.bz2
 
 #Decompress the downloaded data
-bunzip instance-types_en.nt.bz2
-bunzip mappingbased-properties_en.nt.bz2
-bunzip labels_en.nt.bz2
+bunzip instance-types_en.ttl.bz2
+bunzip mappingbased-properties_en.ttl.bz2
+bunzip infobox_properties_en.ttl.bz2
 
 #Make a dump
-cat instance-types_en.nt mappingbased-properties_en.nt labels_en.nt > dump.nt
+cat instance-types_en.ttl mappingbased-properties_en.ttl infobox_properties_en.ttl > dump.nt
